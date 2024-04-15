9c85fb06177bc6b95e52884124a8ea96420e3f87
jo <waxcoin@hotmail.com>
2024-04-15 09:31:18 +0200

    fix: Determine underscore presence

--- srcs/List.c
+++ srcs/List.c
@@ -1,5 +1,11 @@
 #include "../includes/nm.h"
 
+int is_Double_under_score(const char *name) {
+  return name[0] == '_' && name[1] == '_';
+}
+
+int is_Under_score(const char *name) { return name[0] == '_'; }
+
 SymbolNode *AddNode(SymbolNode **head, unsigned long address, char type,
                     const char *name) {
   SymbolNode *new_node = malloc(sizeof(SymbolNode));
@@ -7,14 +13,22 @@ SymbolNode *AddNode(SymbolNode **head, unsigned long address, char type,
     perror("malloc");
     exit(1);
   }
+  if (is_Double_under_score(name)) {
+    new_node->unscore = DOUBLE_UNDER_SCORE;
+  } else if (is_Under_score(name)) {
+    new_node->unscore = UNDER_SCORE;
+  } else {
+    new_node->unscore = NO_UNDER_SCORE;
+  }
   new_node->address = (address == 0) ? LONG_MAX : address;
   new_node->next = NULL;
+  new_node->prev = NULL;
   if (address == 0)
     new_node->address = ULONG_MAX;
   else
     new_node->address = address;
   new_node->type = type;
-  strncpy(new_node->name, name, sizeof(new_node->name) - 1);
+  ft_strcpy(new_node->name, name + new_node->unscore);
   if (!*head) {
     new_node->next = *head;
     *head = new_node;
@@ -25,6 +39,7 @@ SymbolNode *AddNode(SymbolNode **head, unsigned long address, char type,
     }
     new_node->next = current->next;
     current->next = new_node;
+    current->prev = current;
   }
   return new_node;
 }
