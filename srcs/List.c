c6192109676e94fd1bd6d358312a2445a1bf93ac
jo <waxcoin@hotmail.com>
2024-04-16 10:40:27 +0200

    feat: Simplify underscore handling

--- srcs/List.c
+++ srcs/List.c
@@ -1,17 +1,5 @@
 #include "../includes/nm.h"
 
-int is_Double_under_score(const char *name) {
-  return name[0] == '_' && name[1] == '_';
-}
-
-int is_Under_score(const char *name) { return name[0] == '_'; }
-
-int is_Triple_Unscore(const char *name) {
-  return name[0] == '_' && name[1] == '_' && name[2] == '_';
-}
-
-int is_Dot(const char *name) { return name[0] == '.'; }
-
 SymbolNode *GetDuplicate(SymbolNode *head, const char *str) {
   SymbolNode *current = head;
   while (current) {
@@ -32,18 +20,6 @@ SymbolNode *AddNode(SymbolNode **head, unsigned long address, char type,
     exit(1);
   }
   ft_bzero(new_node, sizeof(SymbolNode));
-  if (is_Double_under_score(name)) {
-    new_node->unscore = DOUBLE_UNDER_SCORE;
-    if (is_Triple_Unscore(name)) {
-      new_node->unscore = TRIPLE_UNDER_SCORE;
-    }
-  } else if (is_Under_score(name)) {
-    new_node->unscore = UNDER_SCORE;
-  } else if (is_Dot(name)) {
-    new_node->unscore = DOT;
-  } else {
-    new_node->unscore = NO_UNDER_SCORE;
-  }
   new_node->address = (address == 0) ? LONG_MAX : address;
   new_node->next = NULL;
   new_node->prev = NULL;
@@ -52,8 +28,7 @@ SymbolNode *AddNode(SymbolNode **head, unsigned long address, char type,
   else
     new_node->address = address;
   new_node->type = type;
-  ft_strcpy(new_node->name,
-            name + (new_node->unscore == DOT ? 1 : new_node->unscore));
+  ft_strcpy(new_node->name, name);
   duplicate = GetDuplicate(*head, new_node->name);
   if (duplicate) {
     SymbolNode *tmp = duplicate;
