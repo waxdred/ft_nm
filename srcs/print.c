f7dc24211b9bef6eca0c0e6bf3a22f7b65111a8b
jo <waxcoin@hotmail.com>
2024-04-16 14:00:24 +0200

    feat: Refactor printf calls for better code readability

--- srcs/print.c
+++ srcs/print.c
@@ -1,5 +1,4 @@
 #include "../includes/nm.h"
-#include <stdio.h>
 
 int isPrt(char c) {
   char *str = "RrDTW";
@@ -22,62 +21,49 @@ void printNode(SymbolNode *current, t_flag flag, t_nm *nm) {
     if (flag.u) {
     } else if (current->type == 'A' && current->address != ULONG_MAX) {
       printFlaga(flag);
-      if (nm->type == ELF64)
-        printf("%016lx %c %s\n", current->address, current->type,
-               current->name);
-      else if (nm->type == ELF32)
-        printf("%08lx %c %s\n", (unsigned long)(current->address & 0xFFFFFFFF),
-               current->type, current->name);
-
+      Print_Line_Addr(current->address, current->type, current->name, nm->type,
+                      PRINT_ADDR_SYMBOL);
+    } else if (current->type == 'w') {
+      printFlaga(flag);
+      Print_Line_Addr(current->address, current->type, current->name, nm->type,
+                      PRINT_SYMBOL);
     } else if (current->type == 'U') {
       printFlaga(flag);
-      if (nm->type == ELF64) {
-        printf("                 %c %s\n", current->type, current->name);
-      } else if (nm->type == ELF32) {
-        printf("         %c %s\n", current->type, current->name);
-      }
+      Print_Line_Addr(current->address, current->type, current->name, nm->type,
+                      PRINT_SYMBOL);
     } else if (current->type != 'A') {
       if (current->address != ULONG_MAX) {
         printFlaga(flag);
         if (nm->type == ELF64) {
           if (flag.g && isupper(current->type)) {
-            printf("%016lx %c %s\n", current->address, current->type,
-                   current->name);
+            Print_Line_Addr(current->address, current->type, current->name,
+                            nm->type, PRINT_ADDR_SYMBOL);
           } else if (!flag.g) {
-            printf("%016lx %c %s\n", current->address, current->type,
-                   current->name);
+            Print_Line_Addr(current->address, current->type, current->name,
+                            nm->type, PRINT_ADDR_SYMBOL);
           }
         } else if (nm->type == ELF32) {
           if (flag.g && isupper(current->type)) {
-            printf("%08lx %c %s\n",
-                   (unsigned long)(current->address & 0xFFFFFFFF),
-                   current->type, current->name);
+            Print_Line_Addr(current->address, current->type, current->name,
+                            nm->type, PRINT_ADDR_SYMBOL);
           } else if (!flag.g) {
-            printf("%08lx %c %s\n",
-                   (unsigned long)(current->address & 0xFFFFFFFF),
-                   current->type, current->name);
+            Print_Line_Addr(current->address, current->type, current->name,
+                            nm->type, PRINT_ADDR_SYMBOL);
           }
         }
       } else {
         if (flag.g && isupper(current->type)) {
           printFlaga(flag);
-          if (nm->type == ELF64) {
-            printf("                 %c %s\n", current->type, current->name);
-          } else if (nm->type == ELF32) {
-            printf("         %c %s\n", current->type, current->name);
-          }
+          Print_Line_Addr(current->address, current->type, current->name,
+                          nm->type, PRINT_SYMBOL);
         } else if (isPrt(current->type)) {
           printFlaga(flag);
-          if (nm->type == ELF64)
-            printf("0000000000000000 %c %s\n", current->type, current->name);
-          else if (nm->type == ELF32)
-            printf("00000000 %c %s\n", current->type, current->name);
+          Print_Line_Addr(current->address, current->type, current->name,
+                          nm->type, PRINT_NO_ADDR_SYMBOL);
         } else if (!flag.g) {
           printFlaga(flag);
-          if (nm->type == ELF64)
-            printf("                 %c %s\n", current->type, current->name);
-          else if (nm->type == ELF32)
-            printf("         %c %s\n", current->type, current->name);
+          Print_Line_Addr(current->address, current->type, current->name,
+                          nm->type, PRINT_SYMBOL);
         }
       }
     }
