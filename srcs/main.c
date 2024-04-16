be4f9ada05cd72296fe20be20c6f673ff2024c72
jo <waxcoin@hotmail.com>
2024-04-16 16:11:19 +0200

    feat: Renamed function and improved error handling

--- srcs/main.c
+++ srcs/main.c
@@ -9,47 +9,56 @@ int main(int argc, char **argv) {
   }
 
   if (parse_flags(argc, argv) != 0) {
+    free(nm);
     return 1;
   }
 
   if (argc < 2) {
     print_prg(argv[0], NULL, ERR_NO_PRG);
+    free(nm);
     return 1;
   }
 
-  int fd = open(nm->flags.file, O_RDONLY);
-  if (fd == -1) {
-    print_prg(nm->flags.file, NULL, NULL);
-    return 1;
-  }
-
-  struct stat my_stat;
-  if (stat(nm->flags.file, &my_stat) != 0) {
-    print_prg(argv[0], "stat", NULL);
-    close(fd);
-    return 1;
-  }
-
-  void *map = mmap(NULL, my_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-  if (map == MAP_FAILED) {
-    print_prg(nm->flags.file, "mmap", NULL);
-    close(fd);
-    return 1;
-  }
-
-  nm->flags.elf = get_format(map);
-  if (nm->flags.elf == 0) {
-    print_prg(nm->flags.file, "Erreur", "Format ELF inconnu");
+  Input *input = nm->flags.input;
+  while (input != NULL) {
+    int fd = open(input->name, O_RDONLY);
+    if (fd == -1) {
+      print_prg(input->name, NULL, NULL);
+      break;
+    }
+    struct stat my_stat;
+    if (stat(input->name, &my_stat) != 0) {
+      print_prg(argv[0], "stat", NULL);
+      close(fd);
+      break;
+    }
+    nm->flags.file = input->name;
+    if (nm->flags.nb_file > 1) {
+      ft_putstr("\n");
+      ft_putstr(input->name);
+      ft_putstr(":");
+      ft_putstr("\n");
+    }
+    void *map = mmap(NULL, my_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (map == MAP_FAILED) {
+      print_prg(input->name, "mmap", NULL);
+      close(fd);
+      break;
+    }
+    nm->flags.elf = get_format(map);
+    if (nm->flags.elf == 0) {
+      ft_putstr_fd("ft_nm: ", STDERR_FILENO);
+      ft_putstr_fd(nm->flags.file, STDERR_FILENO);
+      ft_putstr_fd(": file format not recognized\n", STDERR_FILENO);
+      munmap(map, my_stat.st_size);
+      close(fd);
+      break;
+    }
+    nm->Set_Elf();
+    nm->List_symbols(map);
     munmap(map, my_stat.st_size);
     close(fd);
-    return 1;
+    input = input->next;
   }
-  nm->Set_Elf();
-  nm->List_symbols(map);
-
-  munmap(map, my_stat.st_size);
-  close(fd);
-  free(nm);
-
   return 0;
 }
